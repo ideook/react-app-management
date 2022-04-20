c2464d78a05edece3304ec5fdc8440a6e28c4ed5
ideook <ideook@gmail.com>
2022-04-20 21:55:21 +0900

    up

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import { withStyles } from "@mui/styles";
 import CircularProgress from "@mui/material/CircularProgress";
+import CustomerAdd from "./components/CustomerAdd";
 
 const theme = createTheme({
   status: {},
@@ -30,10 +31,25 @@ const styles = {
 };
 
 class App extends Component {
-  state = {
-    customers: "",
-    completed: 0,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: "",
+      completed: 0,
+    };
+    this.stateRefresh = this.stateRefresh.bind(this);
+  }
+
+  stateRefresh() {
+    this.setState({
+      customers: "",
+      completed: 0,
+    });
+
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
@@ -61,48 +77,53 @@ class App extends Component {
     const { classes } = this.props;
 
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {this.state.customers ? (
-              this.state.customers.map((c) => {
-                return (
-                  <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                );
-              })
-            ) : (
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
               <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CircularProgress
-                    className={classes.progress}
-                    variant="determinate"
-                    value={this.state.completed}
-                  />
-                </TableCell>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </Paper>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? (
+                this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      stateRefresh={this.stateRefresh}
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress
+                      className={classes.progress}
+                      variant="determinate"
+                      value={this.state.completed}
+                    />
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </Paper>
+
+        <CustomerAdd stateRefresh={this.stateRefresh} />
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -5,27 +5,24 @@ var corsOptions = {
   origin: "http://localhost:8081",
 };
 app.use(cors(corsOptions));
+
 // parse requests of content-type - application/json
 app.use(express.json());
+
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
+
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
 
 const fs = require("fs");
 const data = fs.readFileSync("./database.json");
 const conf = JSON.parse(data);
-
-//app.get("/api/customers", (req, res) => {
-//  res.json({ message: "Welcome to /api/customers" });
-//});
 
 const config = {
   server: conf.host,
@@ -39,6 +36,8 @@ const config = {
 console.log(config);
 
 const sql = require("mssql");
+const multer = require("multer");
+const upload = multer({ dest: "./upload" });
 
 sql.connect(config, function (err) {
   if (err) {
@@ -49,8 +48,45 @@ sql.connect(config, function (err) {
 
 app.get("/api/customers", (req, res) => {
   var request = new sql.Request();
-  q = "SELECT * FROM CUSTOMER;";
+  q = "SELECT * FROM CUSTOMER WHERE isDeleted = 0";
   request.query(q, (err, rows, fields) => {
     res.send(rows.recordset);
   });
+});
+
+app.use("/image", express.static("./upload"));
+
+app.post("/api/customers", upload.single("image"), (req, res) => {
+  let image = ""; // "/image/" + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  //let params = [image, name, birthday, gender, job];
+
+  // sql.input('image', sql.VarChar, image);
+  // sql.input('name', sql.VarChar, name);
+  // sql.input('birthday', sql.VarChar, birthday);
+  // sql.input('gender', sql.VarChar, gender);
+  // sql.input('job', sql.VarChar, job);
+
+  var request = new sql.Request();
+  q = `INSERT INTO CUSTOMER VALUES ('${image}', '${name}', '${birthday}', '${gender}', '${job}', GETDATE(), 0)`;
+  request.query(q, (err, rows, fields) => {
+    res.send(rows.recordset);
+  });
+});
+
+app.delete("/api/customers/:id", (req, res) => {
+  var request = new sql.Request();
+  //console.log(req.params.id);
+  q = `UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ${req.params.id}`;
+  //console.log(q);
+  request.query(q, (err, rows, fields) => {
+    res.send(rows.recordset);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
